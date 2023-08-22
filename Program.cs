c44423464cd2a24d6fc6ece172fbb0a0d5de7dc5
ENT\burmas3 <subhamoy.burman@medtronic.com>
2023-08-22 22:04:07 +0530

    Day 3

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ internal class Program
         builder.Services.AddScoped<IPlatformRepo,PlatformRepo>();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
